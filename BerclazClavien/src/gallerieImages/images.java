36c32854b758970499d57ead46aba404b1263469
Yann <Yann@YGC>
2017-05-29 21:21:12 +0200

    BDD pour la gallerie et Gallerie d'image

--- BerclazClavien/src/gallerieImages/images.java
+++
@@ -1,6 +0,0 @@
-package gallerieImages;
-
-public class images {
-
-	
-}
--- BerclazClavien/src/interfaceGraphique/interfaceGallerie.java
+++ BerclazClavien/src/interfaceGraphique/interfaceGallerie.java
@@ -1,5 +1,33 @@
 package interfaceGraphique;
 
+import java.awt.Component;
+import java.awt.GridLayout;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
 public class interfaceGallerie extends interfaceGeneral {
 
+	
+	
+	  JPanel panel = new JPanel(new GridLayout(0,1));
+
+	  JButton a = new JButton(new ImageIcon(getClass().getResource("/1.jpg")));
+
+	 
+	
+	  public interfaceGallerie(){
+		
+		  add(panel);
+
+		  panel.add(a);
+
+		  panel.setBounds(30, 30, 340, 610);
+	}
 }
