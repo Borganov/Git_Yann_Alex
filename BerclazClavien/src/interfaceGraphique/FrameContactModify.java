cc22e67d2632b2c3d5323a788a4c1a01a021fd6b
Alexandre Berclaz <Alexandre Berclaz@LENOVO-Y700-Alex>
2017-05-31 22:36:48 +0200

    Mise en place de la liste de contact et du frame pour la modification

---
+++ BerclazClavien/src/interfaceGraphique/FrameContactModify.java
@@ -0,0 +1,85 @@
+package interfaceGraphique;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
+import contact.Contact;
+
+public class FrameContactModify extends interfaceGeneral{
+	
+	
+	JLabel firstnameL = new JLabel("Prénom :");
+	JLabel lastnameL = new JLabel("Nom :");
+	JLabel phoneL = new JLabel("N° de téléphone :");
+	
+	JTextField firstnameF = new JTextField();
+	JTextField lastnameF = new JTextField();
+	JTextField phoneF = new JTextField();
+	
+	JButton save = new JButton("Sauvegarder");
+	
+	private ArrayList<Contact> list;
+	private int ind;
+	
+	public FrameContactModify(int i, ArrayList<Contact> l){
+		list = l;
+		ind = i;
+		//Ajout label prénom
+		add(firstnameL);
+		firstnameL.setBounds(20	,20, 100, 30); //axe x, axe y, largeur, hauteur
+		//Ajout label nom
+		add(lastnameL);
+		lastnameL.setBounds(20	,50, 100, 30); //axe x, axe y, largeur, hauteur
+		//Ajout label phone
+		add(phoneL);
+		phoneL.setBounds(20	,80, 100, 30); //axe x, axe y, largeur, hauteur
+		
+		//Ajout field prénom
+		add(firstnameF);
+		firstnameF.setBounds(130,20, 100, 30); //axe x, axe y, largeur, hauteur
+		firstnameF.setText(list.get(ind).getFirstName());
+		//Ajout field nom
+		add(lastnameF);
+		lastnameF.setBounds(130,50, 100, 30); //axe x, axe y, largeur, hauteur
+		lastnameF.setText(list.get(ind).getLastName());
+		//Ajout field téléphone
+		add(phoneF);
+		phoneF.setBounds(130,80, 100, 30); //axe x, axe y, largeur, hauteur
+		phoneF.setText(list.get(ind).getPhoneNumber());
+		
+		//Ajout boutton sauvegarder
+		add(save);
+		save.setBounds(140, 600, 120, 20);
+		Ecouteurs ecouteur = new Ecouteurs();
+		save.addActionListener(ecouteur);
+		
+	}
+	
+	public class Ecouteurs implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			//si clique sur gallerie
+			if (e.getSource()==save){
+				
+				list.get(ind).setFirstName(firstnameF.getText());
+				list.get(ind).setLastName(lastnameF.getText());
+				list.get(ind).setPhoneNumber(phoneF.getText());
+				
+				
+				
+			}
+			
+		}
+		
+	}
+	
+	
+}
--- BerclazClavien/src/interfaceGraphique/interfaceContactsList.java
+++ BerclazClavien/src/interfaceGraphique/interfaceContactsList.java
@@ -1,22 +1,51 @@
 package interfaceGraphique;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.util.ArrayList;
 
+import javax.swing.JList;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
 import contact.Contact;
+import interfaceGraphique.menuPrincipal.Ecouteurs;
 
 public class interfaceContactsList extends interfaceGeneral{
+
 	//Création de l'array list de contact
-	ArrayList<Contact> contactList = new ArrayList<Contact>();
+	private ArrayList<Contact> contactList = new ArrayList<Contact>();
+	//Création de la listeContact	
+	private JList listContact ;
+	
 	
 	interfaceContactsList() throws ClassNotFoundException, IOException{
 		//Importation des données de BDD_Contact dans l'arrayList
 		DownloadDataContact("./BDD_Contact",contactList);
 		
+		listContact = new JList(contactList.toArray()) ;
+
+		
+		add(listContact);	//placement du bouton sur l'écran
+		listContact.setBounds(10,10, 380, 660); //axe x, axe y, largeur, hauteur
+		
+		//ajout des listener
+		Ecouteurs ecouteur = new Ecouteurs();
+		listContact.addListSelectionListener(ecouteur);
+		
+		
 	}
+	
+	
+	
+	
+	
+	
+	
 	
 	//Méthode de désérialisation des fichiers présent dans le fichier BDD_Contact
 	private static void DownloadDataContact(String path, ArrayList<Contact> listcontact) throws IOException, ClassNotFoundException {
@@ -31,6 +60,21 @@ public class interfaceContactsList extends interfaceGeneral{
 				listcontact.add( (Contact) ois.readObject());
 				ois.close(); 				
 			}
+		}
+		
+	}
+	
+	public class Ecouteurs implements ListSelectionListener{
+
+		@Override
+		public void valueChanged(ListSelectionEvent e) {
+			int contactSelected = (e.getFirstIndex());
+			//instantiation nouvelle fenêtre
+			FrameContactModify frameContactModify = new FrameContactModify(contactSelected, contactList);
+			frameContactModify.setVisible(true);
+			//fermeture de la fênetre actuelle
+			dispose();
+			
 		}
 		
 	}
