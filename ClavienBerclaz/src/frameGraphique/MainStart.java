aaa0a4040b25639f92bb6532de21ccee9ee0a1a4
Alexandre Berclaz <Alexandre Berclaz@LENOVO-Y700-Alex>
2017-06-06 19:15:30 +0200

    suppression none used

--- ClavienBerclaz/src/frameGraphique/MainStart.java
+++ ClavienBerclaz/src/frameGraphique/MainStart.java
@@ -1,15 +1,6 @@
 package frameGraphique;
 
-import java.awt.Graphics;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.util.ArrayList;
-
-import contact.Contact;
 
 public class MainStart {
 
