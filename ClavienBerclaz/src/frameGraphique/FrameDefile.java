45fe0fd8aeb8eb420c4993ea756c43a7a25e8618
Yann <Yann@YGC>
2017-06-09 16:12:15 +0200

    Ajout delete et mise en forme des boutons

--- ClavienBerclaz/src/frameGraphique/FrameDefile.java
+++ ClavienBerclaz/src/frameGraphique/FrameDefile.java
@@ -19,6 +19,7 @@ public class FrameDefile extends FrameGeneral{
 
 	private JButton next = new JButton(new ImageIcon(getClass().getResource("/next.png")));
 	private JButton previous = new JButton(new ImageIcon(getClass().getResource("/previous.png"))); 
+	private JButton delete = new JButton(new ImageIcon(getClass().getResource("/delete.png")));
 
 	
 	public FrameDefile(File [] list, int k, int nombrePhotos){
@@ -29,28 +30,44 @@ public class FrameDefile extends FrameGeneral{
 		panelPhoto.setLayout(null);
 		panelPhoto.setBounds(25, 20, 350, 550);
 
-		
 		add(panelBoutons, BorderLayout.CENTER);
 		panelBoutons.setLayout(null);
 		panelBoutons.setBounds(25,550,350,100);
-
-
+	
 		panelPhoto.add(photo);
 		photo.setBounds(0, 0, 350, 550);
+		photo.setBorder(null);
+		photo.setContentAreaFilled(false);
 		
-		panelBoutons.add(previous);
-		previous.setLayout(null);
-		previous.setBounds(25, 40, 70, 40);
+		panelBoutons.add(delete);
+		delete.setLayout(null);
+		delete.setBounds(135, 40, 70, 40);
+		delete.setBorder(null);
+		delete.setContentAreaFilled(false);
 		
-		panelBoutons.add(next);
-		next.setLayout(null);
-		next.setBounds(250, 40, 70, 40);
+		if(k>0){
+			panelBoutons.add(previous);
+			previous.setLayout(null);
+			previous.setBounds(25, 40, 70, 40);
+			previous.setBorder(null);
+			previous.setContentAreaFilled(false);
+		}
+
+		if(k<nombrePhotos-1){
+			panelBoutons.add(next);
+			next.setLayout(null);
+			next.setBounds(250, 40, 70, 40);
+			next.setBorder(null);
+			next.setContentAreaFilled(false);
+		}
+
 
 		Ecouteurs ecouteur = new Ecouteurs(list, k, nombrePhotos);
 		getBoutonHome().addActionListener(ecouteur);
 		getBoutonOff().addActionListener(ecouteur);
 		next.addActionListener(ecouteur);
 		previous.addActionListener(ecouteur);
+		delete.addActionListener(ecouteur);
 	}
 
 	public class Ecouteurs implements ActionListener{
