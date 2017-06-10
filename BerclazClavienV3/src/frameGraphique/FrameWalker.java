eaa69b8d54bb4662af171dd1f5403e2ad16e0750
Alexandre Berclaz <Alexandre Berclaz@LENOVO-Y700-Alex>
2017-06-10 08:57:37 +0200

    Ajout de la fenêtre de démarage

---
+++ BerclazClavienV3/src/frameGraphique/FrameWalker.java
@@ -0,0 +1,44 @@
+package frameGraphique;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+
+public class FrameWalker extends FrameGeneral{
+	
+	JLabel soft;
+	String list[] = new String[16];
+	Font start = new Font("Verdanan", Font.BOLD, 24);
+	
+	FrameWalker(int i){
+		list[0]="C";
+		list[1]="Cl";
+		list[2]="Cla";
+		list[3]="Cla-";
+		list[4]="Cla-B";
+		list[5]="Cla-Be";
+		list[6]="Cla-Ber";
+		list[7]="Cla-Ber ";
+		list[8]="Cla-Ber S";
+		list[9]="Cla-Ber So";
+		list[10]="Cla-Ber Sof";
+		list[11]="Cla-Ber Soft";
+		list[12]="Cla-Ber Softw";
+		list[13]="Cla-Ber Softwa";
+		list[14]="Cla-Ber Softwar";
+		list[15]="Cla-Ber Software";
+		
+		soft = new JLabel(list[i]);
+		add(soft);
+		soft.setBounds(100, 300, 380, 100);
+		soft.setBackground(null);
+		soft.setFont(start);
+		soft.setForeground(Color.white);
+	}	
+
+}
