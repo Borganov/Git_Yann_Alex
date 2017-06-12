5648983c13bdd37f1f64ba8fe4d09b578da214fa
Yann <Yann@YGC>
2017-06-12 19:34:24 +0200

    test JUnit contactAdd

--- BerclazClavienV3/src/JUnitTest/FrameContactAddTest.java
+++ BerclazClavienV3/src/JUnitTest/FrameContactAddTest.java
@@ -2,13 +2,34 @@ package JUnitTest;
 
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
 import org.junit.Test;
+
+import contact.Contact;
 
 public class FrameContactAddTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testContactControl() throws IOException {
+		
+		Contact ab = new Contact("Alexandre", "Berclaz", "12");
+		Contact yc = new Contact("Yann", "Clavien", "182");
+		
+		Contact test = new Contact("Alexandre", "Berclaz", "12");
+		
+		boolean reponse = false;
+		
+		
+		ArrayList<Contact> testList= new ArrayList<Contact>();
+		testList.add(ab);
+		testList.add(yc);
+		
+		boolean original =test.contactControl(testList);
+		
+		if(original!=reponse)
+		fail("Méthode contactControl fausse");
 	}
 
 }
